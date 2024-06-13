b81cd30393006c57df2dfd9e0949519914d19fda
yz0010 <yz0010@mix.wvu.edu>
2024-06-13 13:19:02 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 002</h2>
+      <p>WVU ID: 800195122</p>
+      <p>Hi I am Yu Zhou</p>
+    </div>
+  );
+}
+
+export default App;
