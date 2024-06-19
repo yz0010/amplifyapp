1c633e0c279077b26e8a35e2b7c8ff10640a473b
yz0010 <yz0010@mix.wvu.edu>
2024-06-19 15:58:55 -0400

    Added Navbar and Card components 02

--- src/Card.js
+++ src/Card.js
@@ -3,35 +3,29 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function Card() {
   return (
-    <div className="container">
+    <div className="container mt-5">
       <div className="row">
-        <div className="col-sm">
-          <div className="card" style={{width: "18rem"}}>
-            <img src="https://via.placeholder.com/150" className="card-img-top" alt="..."/>
+        <div className="col-sm-4">
+          <div className="card text-white bg-primary mb-3">
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h5 className="card-title">Card 01</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
           </div>
         </div>
-        <div className="col-sm">
-          <div className="card" style={{width: "18rem"}}>
-            <img src="https://via.placeholder.com/150" className="card-img-top" alt="..."/>
+        <div className="col-sm-4">
+          <div className="card text-white bg-danger mb-3">
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h5 className="card-title">Card 02</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
           </div>
         </div>
-        <div className="col-sm">
-          <div className="card" style={{width: "18rem"}}>
-            <img src="https://via.placeholder.com/150" className="card-img-top" alt="..."/>
+        <div className="col-sm-4">
+          <div className="card text-white bg-success mb-3">
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h5 className="card-title">Card 03</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
           </div>
         </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <a className="navbar-brand" href="#">Navbar</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -14,12 +14,16 @@ function Navbar() {
             <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
+            <a className="nav-link" href="#">About</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
+            <a className="nav-link" href="#">Contact</a>
           </li>
         </ul>
+        <form className="form-inline ml-auto">
+          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
       </div>
     </nav>
   );
